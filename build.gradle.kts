201d3a50b0285868b30a964386fe237b4b4049a2
stabu_ <buksta@outlook.com>
2025-07-12 18:45:26 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
